f61f3fb2335e08965313b5295148ac9bdd23afa8
Logeshpv2001 <logeshkarthi782@gmail.com>
2025-04-22 16:07:12 +0530

    pwa added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,36 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//     tailwindcss(),
-// })
-
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    [tailwindcss()],
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "robots.txt"],
+      manifest: {
+        name: "My Vite React PWA",
+        short_name: "VitePWA",
+        description: "A React + Vite Progressive Web App",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
