0bfb75032b65bfcf80d0989fcd02d328b38faecd
Logeshpv2001 <logeshkarthi782@gmail.com>
2025-04-24 18:55:09 +0530

    axios changed

--- src/utilities/AxiosInstance.js
+++ src/utilities/AxiosInstance.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://food-track-bend.onrender.com",
   withCredentials: true,
 });
 
 export default AxiosInstance;
-
-// baseURL: "https://food-track-bend.onrender.com",
